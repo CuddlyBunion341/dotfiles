0dfbd6409587c0733ef93f94f709871be4120938
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-28 22:32:57 +0200

    Simplify nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,39 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 vim.opt.title = true
 vim.opt.titlelen = 0
 vim.opt.titlestring = 'nvim %{expand("%:p")}'
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+{ "sainnhe/sonokai", priority = 9999 },
+  "folke/which-key.nvim",
+  "christoomey/vim-tmux-navigator",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+vim.g.sonokai_better_performance = 1
+vim.g.sonokai_style = 'andromeda'
+vim.cmd.colorscheme "sonokai"
+
+
+
