b37518d15bdd7a50d8fd4218f609063fbdbc518d
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-28 22:59:58 +0200

    Update tabstop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,13 @@ vim.opt.titlelen = 0
 vim.opt.titlestring = 'nvim %{expand("%:p")}'
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.wo.number = true
 vim.opt.termguicolors = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -20,16 +26,23 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-{ "sainnhe/sonokai", priority = 9999 },
+  { "sainnhe/sonokai", priority = 9999 },
+  { "nvim-treesitter/nvim-treesitter" },
   "folke/which-key.nvim",
   "christoomey/vim-tmux-navigator",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
 }
 
-local opts = {}
+local opts = {
+  defaults = {
+    lazy = false
+  }
+}
 
 require("lazy").setup(plugins, opts)
+
+vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>')
 
 vim.g.sonokai_better_performance = 1
 vim.g.sonokai_style = 'andromeda'
