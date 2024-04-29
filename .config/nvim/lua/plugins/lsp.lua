8a1942f426a16aa2cf1034966e06ef5606d20fec
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-29 19:51:35 +0200

    Update luasnip config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,152 +1,165 @@
 return {
-	{
-		"ThePrimeagen/refactoring.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("refactoring").setup({})
-		end,
-		keys = {
-			{
-				"<leader>rr",
-				function()
-					require("refactoring").select_refactor()
-				end,
-				desc = "Refactor",
-			},
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		keys = {
-			{ "<leader>m", "<cmd>Mason<cr>" },
-		},
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "solargraph" },
-			})
-		end,
-	},
-	{
-		"nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			-- https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-				callback = function(args)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = args.buf,
-						callback = function()
-							vim.lsp.buf.format({ async = false, id = args.data.client_id })
-						end,
-					})
-				end,
-			})
+  {
+    "ThePrimeagen/refactoring.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup({})
+    end,
+    keys = {
+      {
+        "<leader>rr",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        desc = "Refactor",
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    keys = {
+      { "<leader>m", "<cmd>Mason<cr>" },
+    },
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "solargraph" },
+      })
+    end,
+  },
+  {
+    "nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+        callback = function(args)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ async = false, id = args.data.client_id })
+            end,
+          })
+        end,
+      })
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({ capabilities = capabilities })
-			lspconfig.standardrb.setup({ capabilities = capabilities })
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.standardrb.setup({ capabilities = capabilities })
 
-			lspconfig.solargraph.setup({
-				capabilities = capabilities,
-				cmd = {
-					"asdf",
-					"exec",
-					"solargraph",
-					"stdio",
-				},
-				--   -- settings = {
-				--   --   solargraph = {
-				--   --     autoformat = false,
-				--   --     completion = true,
-				--   --     diagnostic = true,
-				--   --     folding = true,
-				--   --     references = true,
-				--   --     rename = true,
-				--   --     symbols = true
-				--   --   }
-				--   -- }
-			})
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+        cmd = {
+          "asdf",
+          "exec",
+          "solargraph",
+          "stdio",
+        },
+        --   -- settings = {
+        --   --   solargraph = {
+        --   --     autoformat = false,
+        --   --     completion = true,
+        --   --     diagnostic = true,
+        --   --     folding = true,
+        --   --     references = true,
+        --   --     rename = true,
+        --   --     symbols = true
+        --   --   }
+        --   -- }
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.disable({ "rubocop" })
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.diagnostics.erb_format,
-					null_ls.builtins.formatting.erb_lint,
-				},
-			})
-		end,
-		keys = {
-			{ "<leader>gf", vim.lsp.buf.format },
-		},
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		lazy = false,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		lazy = false,
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.disable({ "rubocop" })
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.diagnostics.erb_format,
+          null_ls.builtins.formatting.erb_lint,
+        },
+      })
+    end,
+    keys = {
+      { "<leader>gf", vim.lsp.buf.format },
+    },
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = false,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    }
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 }
