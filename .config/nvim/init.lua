8c7b5d2c16bc855078ceaf48ee825f5f98dffbfd
Daniel Bengl <daniel.bengl@renuo.ch>
2024-05-03 18:01:43 +0200

    Add alt keys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,9 +11,20 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 10
+
+vim.opt.winbar = "%f"
+vim.opt.laststatus = 3
 
 vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>")
+vim.keymap.set("n", "<leader>q!", "<cmd>qa!<cr>")
 vim.keymap.set("n", "<leader>/", "<cmd>noh<cr>")
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+vim.keymap.set("n", "<a-j>", "5jzz")
+vim.keymap.set("n", "<a-k>", "5kzz")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
