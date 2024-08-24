d426474e5d70fd44968a7b65bb5fa48f77ec370c
Daniel Bengl <daniel.bengl@renuo.ch>
2024-08-24 20:28:48 +0200

    Improve symlink script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
 function create_symlink() {
-  if [[ ! -e $2 ]]; then
+  if [[ ! -e $1 ]]; then
+    echo "Source file $1 does not exist"
+  elif [[ ! -e $2 ]]; then
     ln -s $1 $2
     echo "Created symlink $2 -> $1"
   elif [[ -L $2 && ! -e $2 ]]; then
