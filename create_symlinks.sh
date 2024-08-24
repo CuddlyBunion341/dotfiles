c0df6dfe5fc4b2050199fc1c624284a5d122a147
Daniel Bengl <daniel.bengl@renuo.ch>
2024-08-24 20:06:59 +0200

    Added create_symlinks

---
+++ create_symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+function create_symlink() {
+  if [[ ! -e $2 ]]; then
+    ln -s $1 $2
+    echo "Created symlink $2 -> $1"
+  elif [[ -L $2 && ! -e $2 ]]; then
+    echo "Symlink $2 is broken"
+  else
+    echo "Symlink $2 already exists"
+  fi
+}
+
+create_symlink .zshrc ~/.zshrc
