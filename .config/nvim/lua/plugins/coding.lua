d0efbce46bbe7532be8a07aa13a09a070444c67b
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-03 23:17:53 +0200

    Fix linter

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,11 +1,4 @@
 return {
-	{
-		"m4xshen/autoclose.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"nat-418/boole.nvim",
-		event = "VeryLazy",
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -16,37 +9,44 @@ return {
       })
     end
   },
+  {
+    "m4xshen/autoclose.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "nat-418/boole.nvim",
+    event = "VeryLazy",
     opts = {
-				mappings = {
-					increment = "<C-a>",
-					decrement = "<C-x>",
-				},
-				additions = {
-					{ "Foo", "Bar" },
-					{ "tic", "tac", "toe" },
-					{ "->", "<--" },
-					{ "++", "--" },
-					{ "light", "dark" },
-					{ "public", "private", "protected" },
-					{ "if", "unless" },
-					{ "width", "height" },
-					{ "let", "const" },
-				},
-				allow_caps_additions = {
-					{ "enable", "disable" },
-				},
+      mappings = {
+        increment = "<C-a>",
+        decrement = "<C-x>",
+      },
+      additions = {
+        { "Foo",    "Bar" },
+        { "tic",    "tac",     "toe" },
+        { "->",     "<--" },
+        { "++",     "--" },
+        { "light",  "dark" },
+        { "public", "private", "protected" },
+        { "if",     "unless" },
+        { "width",  "height" },
+        { "let",    "const" },
+      },
+      allow_caps_additions = {
+        { "enable", "disable" },
+      },
     }
-	},
-	{
-		"numToStr/Comment.nvim",
+  },
+  {
+    "numToStr/Comment.nvim",
     event = "VeryLazy"
-	},
-	{
-		"machakann/vim-highlightedyank",
-		lazy = false,
+  },
+  {
+    "machakann/vim-highlightedyank",
+    lazy = false,
     config = function()
       vim.g.highlightedyank_highlight_duration = 100
     end
-	},
-	{ "tpope/vim-repeat", lazy = false },
+  },
+  { "tpope/vim-repeat", lazy = false },
 }
