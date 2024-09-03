e68155e98abd4aa49667b775c8c80705d69d5518
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-03 23:17:43 +0200

    Fix vim surround

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,16 +1,21 @@
 return {
 	{
-		"kylechui/nvim-surround",
-		version = "*",
-		lazy = false
-	},
-	{
 		"m4xshen/autoclose.nvim",
 		event = "VeryLazy",
 	},
 	{
 		"nat-418/boole.nvim",
 		event = "VeryLazy",
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
     opts = {
 				mappings = {
 					increment = "<C-a>",
