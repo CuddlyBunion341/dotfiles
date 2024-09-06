4f5d69ced28e65e89c6d3296bedc1014778807ca
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-06 20:33:13 +0200

    Add telescope bindings
    Fix telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  lazy = false
+  keys = {
+    { "<leader>f", "<cmd>Telescope find_files hidden=true<cr>" },
+    { "<leader>w", "<cmd>Telescope live_grep<cr>" },
+    { "<leader>r", "<cmd>Telescope resume<cr>" },
+  }
 }
