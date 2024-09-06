3cf389f7e92d37ad1763c7b39828e8b6b31500e2
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-06 21:41:59 +0200

    Update windowbinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,13 +11,17 @@ vim.opt.scrolloff = 10
 
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "Q", ":quitall<cr>")
+vim.keymap.set("n", "Q", ":quit<cr>")
+vim.keymap.set("n", "<leader>q", ":quitall<cr>")
 vim.keymap.set("n", "<leader>/", "<cmd>noh<cr>")
+vim.api.nvim_set_keymap('n', '<C-z>', '<Nop>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<CR>', { silent = true })
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.keymap.set('n', '<C-k>', ':wincmd k<cr>', { silent = true })
+vim.keymap.set('n', '<C-j>', ':wincmd j<cr>', { silent = true })
+vim.keymap.set('n', '<C-h>', ':wincmd h<cr>', { silent = true })
+vim.keymap.set('n', '<C-l>', ':wincmd l<cr>', { silent = true })
 
 vim.api.nvim_set_keymap('n', '@', '@q', {noremap = true})
 
