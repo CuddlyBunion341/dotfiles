2a53202100ba655bbd2d23a3038afd48beb0ed24
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-06 19:21:03 +0200

    Add theme, basic vim opts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
+vim.g.mapleader = " "
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.termguicolors = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 10
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+vim.api.nvim_set_keymap('n', '@', '@q', {noremap = true})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -5,14 +21,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = { }
+local plugins = {
+  { import = "plugins" }
+}
 
 local opts = { defaults = { lazy = true } }
 
