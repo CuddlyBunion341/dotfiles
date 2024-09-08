4d341b80f5415417d96766975a2106c7d804dca0
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-08 20:48:24 +0200

    Add boole nvim

---
+++ .config/nvim/lua/plugins/boole.lua
@@ -0,0 +1,16 @@
+return {
+	"nat-418/boole.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require('boole').setup({
+			mappings = {
+				increment = '<C-a>',
+				decrement = '<C-x>'
+			},
+			additions = {
+				{'Foo', 'Bar', 'Baz', 'Qux', 'Quux'},
+				{'light', 'dark'}
+			},
+		})
+	end
+}
