1f9471f613de86c12a0274552879a626ef3e125f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-18 21:26:55 +0200

    Update init lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,7 @@ vim.opt.cursorcolumn = true
 vim.opt.termguicolors = true
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.scrolloff = 10
@@ -16,10 +16,13 @@ vim.opt.list = true
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 vim.keymap.set("n", "Q", ":quit<cr>")
+vim.keymap.set("n", "<leader>Q", ":q!<cr>")
 vim.keymap.set("n", "<leader>W", ":wq<cr>")
 vim.keymap.set("n", "<leader>q", ":quitall<cr>")
 vim.keymap.set("n", "<leader>/", "<cmd>noh<cr>")
 vim.keymap.set("n", "<leader>s", ":w<cr>")
+vim.keymap.set("n", "g=", "ggVG=<C-o>")
+vim.keymap.set("n", "zt", "<cmd>%s/\t/  /g<cr><C-o>")
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -30,8 +33,25 @@ vim.keymap.set('n', '<C-l>', ':wincmd l<cr>', { silent = true })
 vim.keymap.set('n', '<C-e>', ':wincmd =<cr>', { silent = true })
 vim.keymap.set('n', '<leader>v', ':wincmd v<cr>', { silent = true})
 vim.keymap.set('n', '<leader>V', ':wincmd s<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>o', function()
+  -- Get the current line content
+  local line = vim.api.nvim_get_current_line()
 
-vim.api.nvim_set_keymap('n', '@', '@q', {noremap = true})
+  -- Extract the string (either single-quoted or double-quoted)
+  local str = line:match("'(.-)'") or line:match('"(.-)"')
+
+  if str then
+    -- Construct the GitHub URL
+    local url = "https://github.com/" .. str
+
+    -- Use the open shell command to open the URL
+    os.execute("open " .. url)
+  else
+    print("No valid string found on the current line.")
+  end
+end)
+
+-- vim.api.nvim_set_keymap('n', '@', '@q', {noremap = true})
 
 vim.keymap.set('n', 'L', ':Lazy<cr>')
 
@@ -73,27 +93,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local numbertoggle_group = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
 
 vim.api.nvim_create_autocmd(
-  {'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter'},
-  {
-    group = numbertoggle_group,
-    pattern = '*',
-    callback = function()
-      if vim.wo.number and vim.fn.mode() ~= 'i' then
-        vim.wo.relativenumber = true
-      end
+{'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter'},
+{
+  group = numbertoggle_group,
+  pattern = '*',
+  callback = function()
+    if vim.wo.number and vim.fn.mode() ~= 'i' then
+      vim.wo.relativenumber = true
     end
-  }
+  end
+}
 )
 
 vim.api.nvim_create_autocmd(
-  {'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave'},
-  {
-    group = numbertoggle_group,
-    pattern = '*',
-    callback = function()
-      if vim.wo.number then
-        vim.wo.relativenumber = false
-      end
+{'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave'},
+{
+  group = numbertoggle_group,
+  pattern = '*',
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = false
     end
-  }
+  end
+}
 )
