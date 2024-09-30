35b9ef4d9ed74a9faf8426780e19558473872256
Daniel Bengl <daniel.bengl@renuo.ch>
2024-09-30 19:16:35 +0200

    Update neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -15,8 +15,10 @@ return {
     })
   end,
   keys = {
+    { "<leader>tt", "<cmd>Neotest run file<cr>" },
     { "<leader>tr", "<cmd>Neotest run<cr>" },
     { "<leader>to", "<cmd>Neotest output<cr>" },
+    { "<leader>tO", "<cmd>Neotest output-panel<cr>" },
     { "<leader>ts", "<cmd>Neotest stop<cr>" },
   }
 }
