8b3f1ca193fac71efab4f7041864fa6bb3d5fb47
CuddlyBunion341 <daniel.bengl@renuo.ch>
2024-10-01 18:35:19 +0200

    Update nvim cmp config

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -7,13 +7,13 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
+    "ray-x/cmp-treesitter",
     "rafamadriz/friendly-snippets",
     "hrsh7th/nvim-cmp",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
   },
   config = function() 
-    -- Set up nvim-cmp.
     local cmp = require('cmp')
 
     cmp.setup({
@@ -21,10 +21,6 @@ return {
         expand = function(args)
           require('luasnip').lsp_expand(args.body)
         end,
-      },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -34,7 +30,8 @@ return {
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
-        -- { name = 'nvim_lsp' },
+        { name = 'nvim_lsp' },
+        { name = 'treesitter' },
         { name = 'luasnip' },
       }, {
         { name = 'buffer' },
@@ -42,18 +39,6 @@ return {
       })
     })
 
-    -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-    -- Set configuration for specific filetype.
-    --[[ cmp.setup.filetype('gitcommit', {
-      sources = cmp.config.sources({
-        { name = 'git' },
-      }, {
-        { name = 'buffer' },
-      })
-    })
-    require("cmp_git").setup() ]]-- 
-
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
@@ -61,7 +46,6 @@ return {
       }
     })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
@@ -72,11 +56,9 @@ return {
       matching = { disallow_symbol_nonprefix_matching = false }
     })
 
-    -- Set up lspconfig.
-    -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-    -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    --   capabilities = capabilities
-    -- }
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require('lspconfig')['lua_ls'].setup {
+      capabilities = capabilities
+    }
   end
 }
