04151dadbf6e4504cd06aa92c81aa7b3d95733cc
Daniel Bengl <daniel@bengl.cz>
2024-10-12 23:38:43 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 OS="$(uname)"
+ADOPT_STOW=$1
 
 if [[ "$OS" == "Darwin" ]]; then
   if ! command -v brew &> /dev/null; then
@@ -10,7 +11,12 @@ if [[ "$OS" == "Darwin" ]]; then
 elif [[ "$OS" == "Linux" ]]; then
   if command -v pacman &> /dev/null; then
     sudo pacman -S bat eza github-cli stow zsh tmux kitty alacritty vim neovim fzf
-    stow alacritty hypr kitty lazygit nixos nvim tmux util zsh
+    if [[ "$ADOPT_STOW" == "--adopt" ]]; then
+      stow --adopt alacritty hypr kitty lazygit nixos nvim tmux util zsh
+    else
+      echo "To adopt the stow command, rerun the script with --adopt as an argument."
+      exit 1
+    fi
   else
     echo "Error: pacman not found."
     exit 1
@@ -29,5 +35,5 @@ if [[ ! -f nvim/.config/nvim/lua/theme.lua ]]; then
 fi
 
 if [[ ! -f alacritty/.config/alacritty/current-theme.toml ]]; then
-  cp alacritty/.config/alacritty/current-theme.toml.example alacritty/.config/alacritt//current-theme.toml
+  cp alacritty/.config/alacritty/current-theme.toml.example alacritty/.config/alacritty/current-theme.toml
 fi
